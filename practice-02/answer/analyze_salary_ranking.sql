e0c76cbf16d8d1222e4bad03e095d46e280fc72f
Shun Fukuda <osunnrnjoy@gmail.com>
2025-12-08 13:49:45 +0900

    practice-02の修正

--- practice-02/answer/analyze_salary_ranking.sql
+++ practice-02/answer/analyze_salary_ranking.sql
@@ -1,32 +1,21 @@
-CREATE OR REPLACE FUNCTION analyze_salary_ranking()
-RETURNS TABLE (
-    department_name VARCHAR,
-    employee_name VARCHAR,
-    salary INT,
-    department_avg_salary INT,
-    salary_rank_in_department INT
-) AS $$
-BEGIN
-    RETURN QUERY --SELECTの結果をテーブルとして返す
-    WITH salary_info AS ( --共通テーブル式CTE
-        SELECT
-            e.id AS employee_id,
-            e.name AS employee_name,
-            e.salary,
-            e.department_id,
-            RANK() OVER (PARTITION BY e.department_id ORDER BY e.salary DESC) AS rank_in_department, --部署の給料高い順
-            AVG(e.salary) OVER (PARTITION BY e.department_id) AS avg_salary --部署の平均給料
-        FROM employees e
-    )
+CREATE OR REPLACE VIEW analyze_salary_ranking  AS
+WITH salary_info AS ( --共通テーブル式CTE
     SELECT
-        d.name AS department_name,
-        s.employee_name,
-        s.salary,
-        ROUND(s.avg_salary)::INT AS department_avg_salary, --平均給与を四捨五入
-        s.rank_in_department::INT AS salary_rank_in_department --部署の給料高い順2
-    FROM salary_info s
-    JOIN departments d
-      ON s.department_id = d.id
-    ORDER BY d.name, s.rank_in_department;
-END;
-$$ LANGUAGE plpgsql;
+        e.id AS employee_id,
+        e.name AS employee_name,
+        e.salary,
+        e.department_id,
+        RANK() OVER (PARTITION BY e.department_id ORDER BY e.salary DESC) AS rank_in_department, --部署の給料高い順
+        AVG(e.salary) OVER (PARTITION BY e.department_id) AS avg_salary --部署の平均給料
+    FROM employees e
+)
+SELECT
+    d.name AS department_name,
+    s.employee_name,
+    s.salary,
+    ROUND(s.avg_salary)::INT AS department_avg_salary, --平均給与を四捨五入
+    s.rank_in_department::INT AS salary_rank_in_department --部署の給料高い順2
+FROM salary_info s
+JOIN departments d 
+  ON s.department_id = d.id
+ORDER BY d.name, s.rank_in_department;
