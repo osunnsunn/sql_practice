626a4c26846b40b2b96b7aacf988f99c60f7cc9e
Shun Fukuda <osunnrnjoy@gmail.com>
2025-12-12 14:10:34 +0900

    practice-01/02の修正

--- practice-01/answer/functions/generate_test_data.sql
+++ practice-01/answer/functions/generate_test_data.sql
@@ -2,7 +2,7 @@ CREATE OR REPLACE FUNCTION generate_test_data(start_date DATE, end_date DATE)
 RETURNS TABLE(generated_order_id INT) AS $$
 DECLARE
     cur_date DATE;
-    product_record RECORD;
+    product_id INT;
     new_order_id INT;
 BEGIN
     FOR cur_date IN SELECT d::date FROM generate_series(start_date, end_date, interval '1 day') AS d LOOP
@@ -10,9 +10,9 @@ BEGIN
         VALUES (cur_date)
         RETURNING order_id INTO new_order_id;
 
-        FOR product_record IN SELECT product_id FROM products LOOP
+        FOR product_id IN SELECT p.product_id FROM products p LOOP
             INSERT INTO order_details(order_id, product_id, quantity)
-            VALUES (new_order_id, product_record.product_id, (FLOOR(RANDOM() * 10) + 1)::int);
+            VALUES (new_order_id, product_id, (FLOOR(RANDOM() * 3) + 1)::int);
         END LOOP;
 
         generated_order_id := new_order_id;
--- practice-02/answer/analyze_salary_ranking.sql
+++ practice-02/answer/analyze_salary_ranking.sql
@@ -1,21 +1,16 @@
-CREATE OR REPLACE VIEW analyze_salary_ranking  AS
+CREATE OR REPLACE VIEW employee_salary_analysis  AS
 WITH salary_info AS ( --共通テーブル式CTE
-    SELECT
-        e.id AS employee_id,
-        e.name AS employee_name,
-        e.salary,
-        e.department_id,
-        RANK() OVER (PARTITION BY e.department_id ORDER BY e.salary DESC) AS rank_in_department, --部署の給料高い順
-        AVG(e.salary) OVER (PARTITION BY e.department_id) AS avg_salary --部署の平均給料
-    FROM employees e
+    SELECT department_id, AVG(salary)::INT AS department_avg_salary
+		FROM employees
+	GROUP BY department_id
 )
 SELECT
     d.name AS department_name,
-    s.employee_name,
-    s.salary,
-    ROUND(s.avg_salary)::INT AS department_avg_salary, --平均給与を四捨五入
-    s.rank_in_department::INT AS salary_rank_in_department --部署の給料高い順2
-FROM salary_info s
-JOIN departments d 
-  ON s.department_id = d.id
-ORDER BY d.name, s.rank_in_department;
+    e.name AS employees_name,
+    e.salary AS employees_salary,
+    s.department_avg_salary,
+	RANK() OVER (PARTITION BY e.department_id ORDER BY e.salary DESC)::INT AS salary_rank_in_department
+FROM employees AS e
+JOIN departments AS d ON e.department_id = d.id
+JOIN salary_info AS s ON e.department_id = s.department_id
+ORDER BY d.name, e.salary DESC;
