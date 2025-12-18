7c51d29ee30af47ac6c81566f5971ebf1a70ad55
Shun Fukuda <osunnrnjoy@gmail.com>
2025-12-18 17:59:21 +0900

    practice-03の提出

--- practice-03/answer/answers.sql
+++ practice-03/answer/answers.sql
@@ -9,7 +9,7 @@ SELECT
 	SUM(o.quantity) AS total_quantity,
 	SUM(o.amount) AS total_amount
 FROM
-	online_sales AS o
+	online_sales AS o;
 
 --課題２
 SELECT
@@ -100,7 +100,87 @@ FROM product_sales AS ps
 JOIN products AS p ON ps.product_id = p.product_id
 ORDER BY sales_rank;
 
---課題７(途中)
-SELECT *
-FROM store_sales AS s
-JOIN online_sales AS o ON o.sale_id = s.sale_id;
+--課題７
+WITH all_sales AS (
+    SELECT
+        c.prefecture AS prefecture,
+        s.amount AS amount
+    FROM store_sales AS s
+    JOIN customers AS c
+        ON c.customer_id = s.customer_id
+    UNION ALL
+    SELECT
+        c.prefecture AS prefecture,
+        o.amount AS amount
+    FROM online_sales AS o
+    JOIN customers AS c
+        ON c.customer_id = o.customer_id
+)
+SELECT
+    prefecture,
+    SUM(amount) AS total_sales_amount
+FROM all_sales
+GROUP BY prefecture;
+
+--課題８
+SELECT
+    c.customer_name,
+	cs.total_amount
+FROM (
+    SELECT
+	    customer_id,
+		SUM(amount) AS total_amount
+	FROM (
+	    SELECT
+		    customer_id,
+			amount
+		FROM store_sales
+		UNION ALL
+		SELECT
+		    customer_id,
+			amount
+		FROM online_sales
+		) AS sales
+    GROUP BY customer_id
+	) AS cs
+JOIN customers AS c
+    ON c.customer_id = cs.customer_id
+ORDER BY cs.total_amount DESC
+LIMIT 1;
+
+--課題９
+SELECT
+    s.store_name,
+    ss.sale_date,
+    ss.amount AS day_amount,
+    SUM(ss.amount) OVER (
+        PARTITION BY ss.store_id
+        ORDER BY ss.sale_date
+    ) AS total_amount 
+FROM store_sales AS ss
+JOIN stores AS s
+    ON s.store_id = ss.store_id
+ORDER BY
+    s.store_name,
+    ss.sale_date;
+
+--課題１０
+SELECT
+    c.gender,
+	SUM(t.total_amount) AS gender_total_amount
+FROM (
+    SELECT
+	    customer_id,
+		SUM(amount) AS total_amount
+	FROM (
+	    SELECT customer_id,amount FROM store_sales
+		UNION ALL
+		SELECT customer_id,amount FROM online_sales
+		) s
+	GROUP BY customer_id
+	) t
+JOIN customers AS c
+    ON c.customer_id = t.customer_id
+GROUP BY c.gender
+ORDER BY gender_total_amount DESC
+LIMIT 1;
