5d4fd7bfda373dc6962725fe2ae3b3c9c9baa2a6
Shun Fukuda <osunnrnjoy@gmail.com>
2025-12-23 18:16:38 +0900

    12/23進捗：カーソル関数を使って処理する方法を調べています

--- practice-04/answer/answers.sql
+++ practice-04/answer/answers.sql
@@ -1,7 +1,36 @@
-WITH RECURSIVE tmp_monthly_date AS
-SELECT 
+CREATE OR REPLACE PROCEDURE calculate_monthly_salary_summary(
+    target_year_month CHAR(7)
+)
+AS $$
+DECLARE
+    month_date = '2025-07';
+
+BEGIN
+
+--曜日をつけた
+CREATE TEMP TABLE temp_work_records AS
+SELECT
     work_id,
     employee_id,
     work_date,
-    (ARRAY['日','月','火','水','木','金','土'])[EXTRACT(DOW FROM work_date)]
-FORM work_records;
+    CASE EXTRACT(DOW FROM work_date)
+        WHEN 0 THEN '日'
+        WHEN 1 THEN '月'
+        WHEN 2 THEN '火'
+        WHEN 3 THEN '水'
+        WHEN 4 THEN '木'
+        WHEN 5 THEN '金'
+        WHEN 6 THEN '土'
+    END AS work_day
+FROM
+    work_records;
+
+SELECT
+	COUNT(*) AS weekday_count,
+FROM
+    temp_work_records
+JOIN temp_work_records AS twr
+    ON e.employee_id = twr.employee_id;
+
+END;
+$$ LANGUAGE plpgsql
