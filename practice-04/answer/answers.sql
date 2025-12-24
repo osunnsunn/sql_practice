d637055e9152ef1a5f669ed75d79a09b18f81c24
Shun Fukuda <osunnrnjoy@gmail.com>
2025-12-24 18:16:45 +0900

    12/24進捗

--- practice-04/answer/answers.sql
+++ practice-04/answer/answers.sql
@@ -1,36 +1,35 @@
 CREATE OR REPLACE PROCEDURE calculate_monthly_salary_summary(
     target_year_month CHAR(7)
 )
+LANGUAGE plpgsql
 AS $$
 DECLARE
-    month_date = '2025-07';
 
 BEGIN
 
---曜日をつけた
+--メモ：一時テーブルに全部の情報入れた
 CREATE TEMP TABLE temp_work_records AS
 SELECT
-    work_id,
-    employee_id,
-    work_date,
-    CASE EXTRACT(DOW FROM work_date)
-        WHEN 0 THEN '日'
-        WHEN 1 THEN '月'
-        WHEN 2 THEN '火'
-        WHEN 3 THEN '水'
-        WHEN 4 THEN '木'
-        WHEN 5 THEN '金'
-        WHEN 6 THEN '土'
-    END AS work_day
-FROM
-    work_records;
+    wr.work_id,
+    wr.employee_id,
+    wr.work_date,
+	wr.hours_worked,
+	CASE
+	    WHEN wr.employee_id = e.employee_id THEN hourly_wage
+		ELSE 000
+	END AS wage,
+	CASE
+	    WHEN wr.shift_type = 'night' THEN e.night_shift_rate --夜勤
+	    WHEN wr.work_date = h.holiday_date THEN e.holiday_rate --休日
+		ELSE 1.00 --平日
+	END AS rate
+FROM work_records AS wr
+JOIN employees AS e ON e.employee_id = wr.employee_id
+LEFT JOIN holidays AS h ON h.holiday_date = wr.work_date
+WHERE wr.work_date BETWEEN '2025-07-01' AND '2025-07-31';
 
-SELECT
-	COUNT(*) AS weekday_count,
-FROM
-    temp_work_records
-JOIN temp_work_records AS twr
-    ON e.employee_id = twr.employee_id;
+OPEN summary_cursor;
+CLOSE summary_cursor
 
 END;
-$$ LANGUAGE plpgsql
+$$
